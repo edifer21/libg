b2bea5d8179f7f484537f9309cd31973b0d95b47
edifer21 <edifer5432@gmailcom>
2024-10-29 17:10:29 +0100

    split error

--- libreria_libft/ft_split.c
+++ libreria_libft/ft_split.c
@@ -88,5 +88,5 @@ char	**ft_split(char const *s, char c)
 	arrstr = malloc((count_c(s, c) + 1) * sizeof(char *));
 	if (!arrstr)
 		return (NULL);
-	return (ft_process_words(s, c, lst));
+	return (ft_process_words(s, c, arrstr));
 }
